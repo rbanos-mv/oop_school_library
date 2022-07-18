5e3b92df0f8a3c945143dc57c37c0e622bebba7f
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-07-18 10:29:40 -0500

    Fix @id initialization

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = null
+    @id = `uuidgen`
     @age = age
     @name = name
     @parent_permission = parent_permission
