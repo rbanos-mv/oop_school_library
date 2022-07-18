b5016cfc2d804a078dc9cfc714fe79eaa99a834d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-07-18 10:40:48 -0500

    Convert Person into Nameable

--- person.rb
+++ person.rb
@@ -1,12 +1,19 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_accessor :age, :name
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = `uuidgen`
     @age = age
     @name = name
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    name
   end
 
   private
