37017209bf241684ed0965daf8492d827d907d6f
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-07-20 03:44:04 -0500

    Add parameters to constructor

--- classroom.rb
+++ classroom.rb
@@ -2,13 +2,13 @@ class Classroom
   attr_accessor :label
   attr_reader :students
 
-  def initialize
-    @label = ''
-    @students = []
+  def initialize(label, students = [])
+    @label = label
+    @students = students
   end
 
   def add_student(student)
-    @students.push(student) unless @students.include?(student)
+    @students << student unless @students.include?(student)
     student.classroom = self
   end
 end
