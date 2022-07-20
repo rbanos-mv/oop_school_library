baf2f8aa166861cbe9db28c809dce0ec62219657
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-07-20 11:34:49 -0500

    Refactor initialize parameters

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
     classroom.add_student(self)
