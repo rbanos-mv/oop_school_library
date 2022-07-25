6dee7dc47d294901e6dc26087d11c514fd85b33a
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-07-25 14:04:52 -0500

    Refactor book

--- book.rb
+++ book.rb
@@ -9,7 +9,7 @@ class Book
   end
 
   def save_rental(rental)
-    @rentals << rental unless @rentals.include?(rental)
+    rentals << rental unless rentals.include?(rental)
   end
 
   def add_rental(person, date)
